719da6db1cbd6fdb6b636e9d4d202b91b1358323
royweb <xytank@gmail.com>
2021-07-19 15:24:28 -0400

    Set up

---
+++ README.md
@@ -0,0 +1,4 @@
+# WRI Meetup
+
+## Notes
+- No need to add `import React from 'react` because it is already added in Next.js
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// 
+function HomePage() {
+    return (
+        <div>
+            <h1>The Home Page</h1>
+        </div>
+    )
+}
+
+export default HomePage;
